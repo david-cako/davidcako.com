90dcee2233ca6e1e9e2e3987b8a0fbac22392906
David Cako <dc@cako.io>
2023-03-23 12:42:02 -0600

    keyboard navigation

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,38 @@
-//spammers deactivate yourselves IRL please
-
-var email = document.getElementsByClassName("email-address");
-var u = "dc"
-var d = "cako.io"
-var a = u + "@" + d
-
-for (i = 0; i < email.length; i++) {
-    email[i].outerHTML = email[i].outerHTML.replace(/name@domain.io/g, a);
+function insertEmailAddress() {
+    var email = document.getElementsByClassName("email-address");
+    var u = "dc"
+    var d = "cako.io"
+    var a = u + "@" + d
+    
+    for (i = 0; i < email.length; i++) {
+        email[i].outerHTML = email[i].outerHTML.replace(/name@domain.io/g, a);
+    }
 }
+
+function navigateLinks(event) {
+    const focusedLink = document.querySelector("#links a:focus");
+    if (event.key === "ArrowUp") {
+        if (focusedLink) {
+            focusedLink.parentElement.previousElementSibling?.firstElementChild.focus();
+        } else {
+            const links = document.querySelectorAll("#links a");
+            links[links.length - 1].focus();
+        }
+    } else if (event.key === "ArrowDown") {
+        if (focusedLink) {
+            focusedLink.parentElement.nextElementSibling?.firstElementChild.focus();
+        } else {
+            const links = document.querySelectorAll("#links a");
+            links[0].focus();
+        }
+    } else if (event.key === "Enter") {
+        if (focusedLink){
+            focusedLink.click();
+        }
+    }
+}
+
+(() => {
+    insertEmailAddress();
+    document.addEventListener("keydown", navigateLinks);
+})();
